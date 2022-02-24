a69d457ed819da94bd075b721e723a8ea2c4ae26
Rafi <mohaymen@Rafis-MacBook-Pro.local>
2022-02-25 00:39:00 +0600

    article page conent added

--- services/index.js
+++ services/index.js
@@ -55,7 +55,7 @@ export const getRecentPosts = async () => {
   return result.posts;
 };
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (slug, categories) => {
   const query = gql`
     query getSimilarPosts($slug: String!, $categories: [String!]) {
       posts(
@@ -75,7 +75,7 @@ export const getSimilarPosts = async () => {
       }
     }
   `;
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query, { slug, categories });
   return result.posts;
 };
 
@@ -91,4 +91,37 @@ export const getCategories = async () => {
   `;
   const result = await request(graphqlAPI, query);
   return result.categories;
+};
+
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug });
+  return result.post;
 };
