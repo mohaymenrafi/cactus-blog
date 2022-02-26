83423981727564a549d297b06bd7e76bbc88d42c
Rafi <mohaymen@Rafis-MacBook-Pro.local>
2022-02-26 07:25:11 +0600

    optional chaining added

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -22,7 +22,7 @@ export default function CategoryPage({ posts }) {
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
+          {posts?.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
         </div>
