7aa0f9b1db7ce8099e16006275398fc615a43951
Rafi <mohaymen@Rafis-MacBook-Pro.local>
2022-02-26 07:35:59 +0600

    type update

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -35,15 +35,15 @@ export default function SinglePostDetails({ post }) {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           <PostDetails post={post} />
-          <Author author={post.author} />
-          <CommentsForm slug={post.slug} />
-          <Comments slug={post.slug} />
+          <Author author={post?.author} />
+          <CommentsForm slug={post?.slug} />
+          <Comments slug={post?.slug} />
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
             <PostWidget
-              slug={post.slug}
-              categories={post.categories.map((item) => item.slug)}
+              slug={post?.slug}
+              categories={post?.categories.map((item) => item.slug)}
             />
             <Categories />
           </div>
